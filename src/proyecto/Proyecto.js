fc6752ad0adc4946ed4a739c9a3db76b15994bf7
mart-dominguez <mdomingu@gmail.com>
2018-11-14 12:45:11 -0300

    x

---
+++ src/proyecto/Proyecto.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Proyecto extends React.Component {
+    render() {
+      return <h1>Hello, {this.props.name}</h1>;
+    }
+}
+
+export default Proyecto;
